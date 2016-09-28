87e1dc6399406e176d666ce2690689078d47027d
Torsten John <lexel.tj@googlemail.com>
2016-09-28 16:15:10 +0200

    Struktur grob erstellt

--- primzahlen.playground/Contents.swift
+++ primzahlen.playground/Contents.swift
@@ -4,3 +4,49 @@ import UIKit
 
 var str = "Hello, playground"
 print(str)
+
+// Beispiel Modulo
+
+var a = 8
+var b = 2
+var erg = a % b
+
+
+// Schleife bis aktuell: 100
+var primzahl:Int8 = 1
+var operant1:Int8 = 1
+var operant2:Int8 = 1
+var ergebnismod:Int8 = 0
+var maxzaehler:Int8 = 10
+var janein:Bool = false
+
+// funktionen
+primzahl  = 1
+func pruefe(prim:Int8) -> Bool {
+    
+    
+    
+    if prim == 1 {
+        janein = true
+    }
+    else {
+        janein = false
+    }
+    print(prim)
+    return janein
+}
+
+// neuer ansatz
+// ------------
+
+// was ist eine Primzahl?
+// - eine Primzahl ist nur durch 1 und sich selber teilbar
+// - zusätzlich prüfen wir ob das ergebnis eine ganzzahl ist
+// - beides muss passen
+
+while primzahl <= maxzaehler {
+   var test:Bool = pruefe( prim: primzahl)
+    primzahl += 1
+}
+
+
